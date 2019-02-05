c766fed82c10e565a08b9e848c5219302b691db8
immaculyne <46965166+immaculyne@users.noreply.github.com>
2019-02-06 00:42:15 +0530

    Update prime _no.py

--- prime _no.py
+++ prime _no.py
@@ -1,10 +1,10 @@
 a=int(input())
-K=0
+k=0
 for i in range(2,a//2+1):
 	if(a%i==0):
-		K=K+1
-		
-if(K<=0):
-	print(a "is prime number")
+		k=k+1
+if(k<=0):
+	print("Prime number")
 else:
-	print("Not a prime number") 
+	print("Not a prime number")
+	

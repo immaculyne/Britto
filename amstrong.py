262c9cb6fa80e1964f32318b6dc4152248da9f8c
immaculyne <46965166+immaculyne@users.noreply.github.com>
2019-02-06 01:12:41 +0530

    Update amstrong.py

--- amstrong.py
+++ amstrong.py
@@ -1,11 +1,11 @@
-x=int(raw_input())
+N=int(raw_input())
 n=len(str(x))
 sum=0
-if (x>0):
+if (N>0):
 	digit=x%10
 	sum=+digit**n
 	x//=10
-if x==sum:
+if (N==sum):
 	print("amstrong")
 else:
 	print("Not")

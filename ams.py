197adea254305b7c46a2e95d20ae8aa8d36008da
immaculyne <46965166+immaculyne@users.noreply.github.com>
2019-02-13 09:14:41 +0000

    Create ams.py

---
+++ ams.py
@@ -0,0 +1,13 @@
+x,n=map(int,raw_input().split())
+
+for num in range(x,n+1):
+   sum = 0
+   temp = num
+   while temp > 0:
+       digit = temp % 10
+       b=digit**3
+       sum=sum+b
+       temp //= 10
+
+   if num == sum:
+       print(num,end" ")
